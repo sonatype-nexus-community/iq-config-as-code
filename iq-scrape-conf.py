faf9c4f14865b8c9ec35ccb49560fd430fc97a0c
Kevin Matthews <kmatthews@sonatype.com>
2021-03-23 12:36:01 +0000

    Enable 'scrape' of specific organisation(s) and/or application(s).

--- iq-scrape-conf.py
+++ iq-scrape-conf.py
@@ -52,7 +52,13 @@ def get_arguments():
 
     debug = args["debug"]
     self_signed = args["self_signed"]
-    entities = args["scope"].replace(" ", "").split(",")
+    entities = args["scope"].split(",")
+    # Remove outer whitespace from entity (org, app)
+    for entity in entities:
+        if entity.strip() not in entities:
+            entities.remove(entity)
+            entities.append(entity.strip())
+
     iq_session = requests.Session()
     iq_session.cookies.set('CLM-CSRF-TOKEN', 'api')
     iq_session.headers = {'X-CSRF-TOKEN': 'api'}
