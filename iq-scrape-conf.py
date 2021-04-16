6d81aff58180ade661e614df2403036aa56953ed
Kevin Matthews <kmatthews@sonatype.com>
2021-04-16 13:16:21 +0100

    Bug Fix - identified by Muse-Dev scanning

--- iq-scrape-conf.py
+++ iq-scrape-conf.py
@@ -54,10 +54,15 @@ def get_arguments():
     self_signed = args["self_signed"]
     entities = args["scope"].split(",")
     # Remove outer whitespace from entity (org, app)
+    entities2 = []
+
     for entity in entities:
         if entity.strip() not in entities:
-            entities.append(entity.strip())
+            entities2.append(entity.strip())
+        else:
+            entities2.append(entity)
 
+    entities = entities2
     iq_session = requests.Session()
     iq_session.cookies.set('CLM-CSRF-TOKEN', 'api')
     iq_session.headers = {'X-CSRF-TOKEN': 'api'}
